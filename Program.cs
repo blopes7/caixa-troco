885050278e16f43984a9968cf3eb23c2108d5801
FLAVIO CELESTINO <81762067+punk23@users.noreply.github.com>
2021-06-15 23:09:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace caixa_troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do produto: ");
+            string valorDigitado1 = Console.ReadLine();
+            if (valorDigitado1 == string.Empty)
+            {
+                Environment.Exit(0);
+            }
+            int valorProduto = Convert.ToInt32(valorDigitado1);
+
+            Console.WriteLine("Digite o valor do pagamento: ");
+            string valorDigitado2 = Console.ReadLine();
+            int valorPagamento = Convert.ToInt32(valorDigitado2);
+
+            if (valorProduto < valorPagamento)
+            {
+                int troco = valorPagamento - valorProduto;
+                Console.WriteLine("O seu troco é de:R$ " + troco);
+            }
+
+            else
+            {
+                Console.WriteLine("Não tem troco bello!");
+            }
+        }
+    }
+}
